7c6c5ef8860b627c6cee05e52afa3be6b9bd4d86
Ilya <zaz128@yandex.ru>
2022-08-16 23:34:15 +0300

    Переписан скрипт в соотв. с переименованием каталогов

--- bmake.bat
+++ bmake.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
-gcc -m32 FileEncoderFM\main.c FileEncoderFM\utils.c -o BIN2FM.EXE
-gcc -m32 StreamDecoderFM\main.c FileEncoderFM\utils.c -o FM2BIN.EXE
-gcc -m32 wavConverter\exConverter\main.c -o WAVCONV.EXE
-gcc -m32 wavConverter\exHeader\main.c  wavConverter\exHeader\debug.c -o WAVHEAD.EXE
-gcc -m32 wavConverter\exStitcher\main.c -o WAVBIND.EXE
+gcc -m32 BIN2FM\main.c BIN2FM\utils.c -o BIN2FM.EXE
+gcc -m32 FM2BIN\main.c FM2BIN\utils.c -o FM2BIN.EXE
+gcc -m32 WAVCONV\main.c -o WAVCONV.EXE
+gcc -m32 WAVHEAD\main.c  WAVHEAD\debug.c -o WAVHEAD.EXE
+gcc -m32 WAVBIND\main.c -o WAVBIND.EXE
